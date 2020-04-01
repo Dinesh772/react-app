f739d8a2b9874fffdc95d57f912b7bb08eef915b
Ganesh karedla <ganeshkaredla6@gmail.com>
2020-04-01 15:10:13 +0000

    added assets

--- src/components/Page1/index.js
+++ src/components/Page1/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Page1() {
-  return <h1>Page 1</h1>;
+  return( 
+      <div className="text-white bg-black h-screen flex justify-center items-center ">
+      <h1>Page 1</h1>
+      </div>
+      );
 }
 
 export default Page1;
+//text-6xl`
