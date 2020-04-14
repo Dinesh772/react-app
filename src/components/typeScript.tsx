9338fd21caf18457a26b122f52c07aa9b1733fdc
Ganeshkaredla <ganeshkaredla6@gmail.com>
2020-04-14 17:12:03 +0530

    added assets

--- src/components/typeScript.tsx
+++ src/components/typeScript.tsx
@@ -4,26 +4,12 @@ import { observer } from "mobx-react"
 
 @observer
 class TypeScript extends React.Component{
-    @observable object={
-        name:'some name',
-        city:'some city',
-        age:200,
-    }
-
-
-    handleClick=()=>{
-        this.object.name='Ganesh';
-        this.object.city='Rjy';
-        this.object.age=22;
-    }
+        @observable count='asdads'
     render(){
         return(
         <div>
-            <p>{this.object.name}</p>
-            <p>{this.object.city}</p>
-            <p>{this.object.age}</p>
-            <button onClick={this.handleClick}>click me</button>
-            </div>
+           
+        </div>
         )
     }
 }
